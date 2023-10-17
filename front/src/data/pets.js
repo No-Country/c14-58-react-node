f3457641220665f4143f1974687ff4a05359f246
Oliver <opaucarq@uni.pe>
2023-10-17 15:07:05 -0500

    add: url photo to data

---
+++ front/src/data/pets.js
@@ -0,0 +1,138 @@
+
+export function pets(){
+
+  let nextId = 1;
+  const data = [
+    {
+      id: nextId++,
+      type: "Found",
+      photo:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRJIUmoPn2I-mNRpUXrKcgukbe_rgAxyCXTA&usqp=CAU",
+      role: "tenant",
+      active: false,
+    },
+    {
+      id: nextId++,
+      type: "Lost",
+      photo:
+      "https://okdiario.com/img/2023/04/10/-es-malo-que-los-gatos-coman-moscas-y-mosquitos-635x358.jpg",
+      role: "tenant",
+      active: false,
+    },
+    {
+      id: nextId++,
+      type: "Found",
+      photo:
+      "https://mott.pe/noticias/wp-content/uploads/2016/04/Estos-perros-no-podr%C3%ADan-lucir-m%C3%A1s-adorables-en-esta-tierna-sesi%C3%B3n-de-fotos-1280x720.jpg",
+      role: "tenant",
+      active: false,
+    },
+    {
+      id: nextId++,
+      type: "Lost",
+      photo:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-m2vwkOIl0XUwGw3nKHlU_NkAnrpHVfY4UQ&usqp=CAU",
+      role: "tenant",
+      active: false,
+    },
+    {
+      id: nextId++,
+      type: "Lost",
+      photo:
+      "https://c.files.bbci.co.uk/48DD/production/_107435681_perro1.jpg",
+      role: "tenant",
+      active: false,
+    },
+    {
+      id: nextId++,
+      type: "Found",
+      photo:
+      "https://fotografias.lasexta.com/clipping/cmsimages01/2022/08/09/3FFA8546-05CE-4608-9B69-6602D02A4C58/cachorro-pomsky_103.jpg",
+      role: "tenant",
+      active: false,
+    },
+    {
+      id: nextId++,
+      type: "Found",
+      photo:
+      "https://purina.com.pe/sites/default/files/2022-10/purina-brand-que-sabes-de-los-perros-poodle.jpg",
+      role: "tenant",
+      active: false,
+    },
+    {
+      id: nextId++,
+      type: "Lost",
+      photo:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrdFFzI4CywaWbEiSHgJS5in3hmWXNUNPFBA&usqp=CAU",
+      role: "tenant",
+      active: false,
+    },
+    {
+      id: nextId++,
+      type: "Lost",
+      photo:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtDixL9qR97cxb-K7jApucgkzLPOHOXkUlTA&usqp=CAU",
+      role: "tenant",
+      active: false,
+    },
+    {
+      id: nextId++,
+      type: "Found",
+      photo:
+      "https://mott.pe/noticias/wp-content/uploads/2016/04/Estos-perros-no-podr%C3%ADan-lucir-m%C3%A1s-adorables-en-esta-tierna-sesi%C3%B3n-de-fotos-1280x720.jpg",
+      role: "tenant",
+      active: false,
+    },
+    {
+      id: nextId++,
+      type: "Lost",
+      photo:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ34mY38PSRN_AHDNG_xNpOQJVQMm44sUy8mw&usqp=CAU",
+        role: "tenant",
+        active: false,
+      },
+      {
+        id: nextId++,
+        type: "Found",
+        photo:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBpvWLBfQdOaEWE3A2H8AlosEjsYnrwx1Eyw&usqp=CAU",
+        role: "tenant",
+        active: false,
+      },
+      {
+        id: nextId++,
+        type: "Found",
+        photo:
+        "https://mivet.com/hubfs/shutterstock_2057390060.jpg",
+        role: "tenant",
+        active: false,
+      },
+      {
+        id: nextId++,
+        type: "Lost",
+        photo:
+        "https://estaticos-cdn.prensaiberica.es/clip/b84dc4c1-d4db-4bd4-896c-37986d62b6b4_16-9-discover-aspect-ratio_default_0.jpg",
+        role: "tenant",
+        active: false,
+      },
+      {
+        id: nextId++,
+        type: "Found",
+        photo:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzaS0EwJ--inZ2UfnDJoxfZOdDPVFuW01v-g&usqp=CAU",
+        role: "tenant",
+        active: false,
+      },
+      {
+        id: nextId++,
+        type: "Lost",
+        photo:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTkZHEFkORC_oLhr2Cl01IiztoIdmkZKOlAw&usqp=CAU",
+        role: "tenant",
+        active: false,
+      },
+      
+  ];
+  return data;
+}
+
