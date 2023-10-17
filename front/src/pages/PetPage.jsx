1995271dde8590adacc86882dab35b093dc568a6
Oliver <opaucarq@uni.pe>
2023-10-17 18:08:56 -0500

    add: petPage describe one pet

--- front/src/pages/PetPage.jsx
+++ front/src/pages/PetPage.jsx
@@ -1,7 +1,27 @@
-
+import React from 'react'
+import ImageSlider from '../components/ImageSlider'
+import Header from '../components/Header'
+import Footer from '../components/Footer/Footer'
+Image
 function PetPage() {
   return (
-    <div>PetPage</div>
+    <div>
+      <Header/>
+      <h1 className='text-6xl'>PetPage</h1>
+      <ImageSlider/>
+      <h3  className='text-4xl'>Descripción de la mascota</h3>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi, animi exercitationem. Inventore aspernatur itaque ex perspiciatis deleniti distinctio incidunt totam soluta placeat magnam dicta non odio tempora, ratione nihil autem!</p>
+      <br/>
+      <h3  className='text-2xl'>Contacto</h3>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi, animi exercitationem. Inventore aspernatur itaque ex perspiciatis deleniti distinctio incidunt totam soluta placeat magnam dicta non odio tempora, ratione nihil autem!</p>
+      <h3>Descripción de la mascota</h3>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi, animi exercitationem. Inventore aspernatur itaque ex perspiciatis deleniti distinctio incidunt totam soluta placeat magnam dicta non odio tempora, ratione nihil autem!</p>
+      <br/>
+      <h3>Descripción de la mascota</h3>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi, animi exercitationem. Inventore aspernatur itaque ex perspiciatis deleniti distinctio incidunt totam soluta placeat magnam dicta non odio tempora, ratione nihil autem!</p>
+      <br/>
+      <Footer/>
+    </div>
   )
 }
 
