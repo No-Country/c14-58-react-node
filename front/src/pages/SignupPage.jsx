895b17d740d30933becf99c0d869d11d0b79c2c3
Oliver <opaucarq@uni.pe>
2023-10-17 11:29:10 -0500

    add: signuppage

---
+++ front/src/pages/SignupPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../components/Header'
+import Footer from '../components/Footer/Footer'
+import Signup from '../components/Login/Signup'
+
+function SignupPage() {
+  return (
+    <>
+      <Header />
+      <Signup />
+      <Footer />
+    </>
+  )
+}
+
+export default SignupPage
