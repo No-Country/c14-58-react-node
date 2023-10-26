687e97d01d6fbfffbaf6b37afa0a61e503434fb2
OliverP <opaucarq@uni.pe>
2023-10-26 13:23:58 -0500

    redirect to home using token

--- front/src/pages/LandingPage.jsx
+++ front/src/pages/LandingPage.jsx
@@ -1,10 +1,19 @@
+import { useEffect } from "react";
 import About from "../components/About/About";
 import ListCards from "../components/AnimalsSection/ListCards";
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header";
 import HeroSection from "../components/HeroSection";
+import { useNavigate } from "react-router-dom";
 
 function LandingPage() {
+  const token = localStorage.getItem("token");
+  const navigate = useNavigate()
+  useEffect(()=>{
+    if (token) {
+      navigate("/home");
+    }
+  },[])
   return (
     <>
       <Header />
