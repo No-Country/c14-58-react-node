7d7fd18c4c0ece0f5ff993125cb40e7004b1f894
OliverP <opaucarq@uni.pe>
2023-10-26 13:18:17 -0500

    update card props with values from the api

--- front/src/components/AnimalsSection/Card/Card.jsx
+++ front/src/components/AnimalsSection/Card/Card.jsx
@@ -9,18 +9,18 @@ export function Card({ data }) {
 
   return (
     <div className="card" style={{cursor:"pointer"}} onClick={()=>navigate('/pets/2')}>
-      <img src={data.photo} alt="pet" className="card__photo" />
-      <div className="card__type" style={{background:`${data.type == "Found" ? "green" : "red"}`}}>
+      <img src={data.image} alt="pet" className="card__photo" />
+      <div className="card__type" style={{background:`${data.status == "found" ? "green" : "red"}`}}>
         <IconContext.Provider value={{ size: "1.25rem" }}>
         <FaPaw />
         </IconContext.Provider>
-        <p className="card__type-sale" style={{fontSize: "32px"}}> {data.type}</p>
+        <p className="card__type-sale" style={{fontSize: "32px"}}> {data.status}</p>
       </div>
       <div>
         <div className="card__content">
           <div className="card__descriptions" style={{display:"flex", gap:"0px"}}>
-            <p>86872 Jacob Gateway, Durganport, WV 48044</p>
-            <p> CARACTERISTICAS</p>
+            <h4 className="font-semibold text-2xl">{data.title}</h4>
+            <p>{data.description}</p>
 
           </div>
         </div>
--- front/src/components/AnimalsSection/ListCards.jsx
+++ front/src/components/AnimalsSection/ListCards.jsx
@@ -2,7 +2,7 @@ import styled from "@emotion/styled";
 import Button from "../../ui/Button";
 import { Card } from "./Card/Card";
 import { Link } from "react-router-dom";
-// import usePets from "../../hooks/usePets";
+import { useSelector } from "react-redux";
 
 const Container = styled.div`
   display: flex;
@@ -10,7 +10,7 @@ const Container = styled.div`
   align-items: center;
   text-align: center;
   gap: 32px;
-  padding: 64px 100px;
+  padding: 16px 100px;
 `;
 const CardContainer = styled.div`
   display: flex;
@@ -21,104 +21,26 @@ const CardContainer = styled.div`
   }
 `;
 
-const CardContainer2 = styled.div`
-  display: none;
-  gap: 48px;
-
-  @media (max-width: 1200px) {
-    display: flex;
-    flex-direction: column;
-  }
-`;
-const Header = styled.div`
-  display: flex;
-  gap: 16px;
-  flex-direction: column;
-  h2 {
-    font-family: "Montserrat";
-    font-size: 14px;
-    font-weight: 500;
-    line-height: 20px;
-    letter-spacing: 0.1px;
-    color: #373737;
-  }
-  h3 {
-    font-family: "Montserrat";
-    font-size: 36px;
-    font-weight: 400;
-    line-height: 48px;
-    letter-spacing: 0.25px;
-    color: #bf5f82;
-  }
-`;
-
-const data1 = {
-  type: "Lost",
-  photo:
-    "https://purina.com.pe/sites/default/files/styles/webp/public/2022-10/purina-que-sabes-de-los-perros-poodle_0.jpg.webp?itok=pi6SunYq",
-  role: "tenant",
-  active: false,
-};
-const data2 = {
-  type: "Found",
-  photo:
-    "https://images.unsplash.com/photo-1517423440428-a5a00ad493e8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60",
-  role: "tenant",
-  active: false,
-};
-const data3 = {
-  type: "Found",
-  photo:
-    "https://mymodernmet.com/wp/wp-content/uploads/2018/11/dogs-catching-treats-christian-vieler-12.jpg",
-  role: "tenant",
-  active: false,
-};
-const data4 = {
-  type: "Found",
-  photo:
-    "https://as01.epimg.net/diarioas/imagenes/2022/05/29/actualidad/1653826510_995351_1653826595_noticia_normal_recorte1.jpg",
-  role: "tenant",
-  active: false,
-};
-const data5 = {
-  type: "Lost",
-  photo:
-    "https://mott.pe/noticias/wp-content/uploads/2016/04/Estos-perros-no-podr%C3%ADan-lucir-m%C3%A1s-adorables-en-esta-tierna-sesi%C3%B3n-de-fotos-1280x720.jpg",
-  role: "tenant",
-  active: false,
-};
 
 function ListCards() {
-  // const { pets, loading } = usePets();
+  const {loading} = useSelector(state => state.user)
+  const data = useSelector(state => state.user.user)
+  console.log(data?.Pets)
 
   return (
     <Container>
-      {/* {loading ? (
-        <div>Loading</div>
-      ) : (
-        pets.map((pet) => {
-          return (
-            <div key={pet.id}>
-              <h3>{pet.title}</h3>
-              <img alt={pet.title} src={pet.image} />
-              <p> {pet.description} </p>
-            </div>
-          );
-        })
-      )} */}
+      <h1 className="text-3xl font-extrabold">Your Pets</h1>
       <CardContainer>
-        <Card data={data1} />
-        <Card data={data2} />
-        <Card data={data3} />
-      </CardContainer>
+      {
+
+        data?.Pets.map(pet=>(<Card key={data.id} data={pet} />))
+      }
+        </CardContainer>
+
       <Button type="primary" size="large">
         <Link to="/pets">Otras mascostas perdidas</Link>
       </Button>
-      <CardContainer2>
-        <Card data={data4} />
 
-        <Card data={data5} />
-      </CardContainer2>
     </Container>
   );
 }
