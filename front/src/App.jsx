d7173f313518c8bad14df7b0fc6a0b164784bcea
OliverP <opaucarq@uni.pe>
2023-10-26 13:18:45 -0500

    getUser for refresh pages

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,8 +2,17 @@ import './App.css'
 import { RouterProvider } from "react-router-dom";
 import router from './routes';
 
+import { useEffect } from "react";
+import { useDispatch } from 'react-redux';
+import { getUser } from './redux/slices/user';
+
+// const navigate = useNavigate();
 
 function App() {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(getUser())
+  }, [dispatch]);
 
   return (
     <RouterProvider router={router} />
