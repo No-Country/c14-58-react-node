50ed68d93ed0a1912d406e7b2bcc8ac7b29ea31c
OliverP <opaucarq@uni.pe>
2023-10-26 13:19:16 -0500

    Include state from store to hide login and signup

--- front/src/components/Header/Header.jsx
+++ front/src/components/Header/Header.jsx
@@ -1,8 +1,10 @@
 import styled from "@emotion/styled";
 import Button from "../../ui/Button";
-import { Link } from "react-router-dom";
-import Search from "../../ui/Search";
+import { Link, useNavigate } from "react-router-dom";
 import { FaBars } from "react-icons/fa"; // Importa el ícono de hamburguesa
+import { useDispatch, useSelector } from "react-redux"
+import { logoutUser } from "../../redux/slices/user";
+
 
 const Container = styled.div`
   position: sticky;
@@ -46,6 +48,15 @@ const HamburgerMenu = styled.div`
 `;
 
 export default function Header() {
+  const {user} = useSelector(state => state.user)
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const handleLogout = () =>{
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    dispatch(logoutUser())
+    navigate('/')
+  }
   return (
     <Container>
       <Navbar>
@@ -64,16 +75,35 @@ export default function Header() {
           <li><Link to="/contact">Contact</Link></li>
           <li>
             <div style={{ display: "flex", gap: "8px" }}>
-              <Link to="/login">
-                <Button type="secondary" size="small">
-                  Login
-                </Button>
-              </Link>
-              <Link to="/signup">
-                <Button type="primary" size="small">
-                  Signup
-                </Button>
-              </Link>
+              {
+                user ? (
+                  <>
+                  <p style={{fontWeight:"800"}}>
+                    Hi {user.name.charAt(0).toUpperCase() + user.name.slice(1)} 
+                  </p>
+                  <button
+                    onClick={() => handleLogout()} // Llama a la función handleLogout al hacer clic
+                    style={{ cursor: "pointer" }}
+                  >
+                    Logout
+                  </button>
+                  </>
+                ) : (
+                  <>
+                    <Link to="/login">
+                      <Button type="secondary" size="small">
+                        Login
+                      </Button>
+                    </Link>
+                    <Link to="/signup">
+                      <Button type="primary" size="small">
+                        Signup
+                      </Button>
+                    </Link>
+                  </>
+                )
+              }
+              
             </div>
           </li>
         </Options>
