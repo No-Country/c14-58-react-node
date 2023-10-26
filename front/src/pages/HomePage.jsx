fa697be2d9b7ca67be01fec29fa04e1fc8d212ab
OliverP <opaucarq@uni.pe>
2023-10-26 13:20:42 -0500

    HomePage for userSession

---
+++ front/src/pages/HomePage.jsx
@@ -0,0 +1,18 @@
+import ListCards from "../components/AnimalsSection/ListCards";
+import Footer from "../components/Footer/Footer";
+import Header from "../components/Header";
+import HeroSection from "../components/HeroSection";
+
+function HomePage() {
+  return (
+    <>
+      <Header />
+      <HeroSection/>
+      
+      <ListCards />
+      <Footer />
+    </>
+  );
+}
+
+export default HomePage;
