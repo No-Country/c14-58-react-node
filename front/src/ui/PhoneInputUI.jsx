a1b06073fe5a69c23ca58382513aac64ed18ea28
OliverP <opaucarq@uni.pe>
2023-10-31 13:52:49 -0500

    add: PhoneInputUI component

---
+++ front/src/ui/PhoneInputUI.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react"
+import PhoneInput from "react-phone-input-2"
+import "react-phone-input-2/lib/style.css"
+import styled from "@emotion/styled"
+
+const PhoneInput2 = styled(PhoneInput)`
+  .react-tel-input {
+  color: #373737;
+  }
+
+  .form-control{
+    height: 42px;
+
+    width: 100%;
+    border-color: #f48fb1;
+    font-family: Inter;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 24px;
+    letter-spacing: 0.5px;
+    color: #444;
+    border-color: #f48fb1;
+    border-radius: 8px;
+  }
+  .flag-dropdown{
+  }
+`
+
+function PhoneInputUI({setPhone}) {
+  const [phoneNumber, setPhoneNumber] = useState('')
+  const handleChange = (value) => {
+    setPhone(value)
+    setPhoneNumber(value)
+  }
+  const validatePhoneNumer = (phoneNUmber) => {
+    const phoneNumberPattern = /^\d{10}$/
+    return phoneNumberPattern.test(phoneNUmber)
+  }
+
+  return (
+    <div>
+      <label>
+        <PhoneInput2
+          className="w-full"
+          country = {'us'}
+          value={phoneNumber}
+          onChange={handleChange}
+          inputProps={{required:true}}
+        />
+      </label>
+    </div>
+  )
+}
+
+export default PhoneInputUI
