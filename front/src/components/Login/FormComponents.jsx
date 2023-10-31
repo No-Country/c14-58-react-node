f3bdfa1b9b03850dad4370ef279ad0bd0f329eb6
OliverP <opaucarq@uni.pe>
2023-10-31 10:01:14 -0500

    add: list components for the forms

---
+++ front/src/components/Login/FormComponents.jsx
@@ -0,0 +1,59 @@
+import styled from "@emotion/styled";
+
+export const ContainerSignup = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: start;
+  padding-top: 100px;
+`;
+export const Input = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: #373737;
+  gap: 4px;
+  label {
+    font-family: Inter;
+    font-size: 10px;
+    font-weight: 400;
+    line-height: 12px;
+    letter-spacing: 1.5px;
+    text-align: left;
+  }
+  input {
+    font-family: Inter;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 24px;
+    letter-spacing: 0.5px;
+    text-align: left;
+    color: #8e8e8e;
+    padding: 8px;
+    border-color: #f48fb1;
+    border-radius: 8px;
+  }
+`;
+export const FormContainer = styled.div`
+  max-width: 600px;
+  width: 200%;
+  background-color: white;
+  padding: 32px;
+  padding-bottom: 48px;
+
+  margin: 0 auto;
+`;
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+
+`;
+export const TitleForm = styled.h1`
+  text-align: center;
+  font-family: Montserrat;
+  font-size: 24px;
+  font-weight: 400;
+  line-height: 32px;
+  letter-spacing: 0px;
+  margin-bottom: 50px;
+`;
