1497fc56853be016bfde2c47733e679cc9235a4f
herancur <hernan.cur@hotmail.com>
2023-11-01 20:52:03 -0300

    Connected front>back

--- front/src/components/PromptAndResponse/index.jsx
+++ front/src/components/PromptAndResponse/index.jsx
@@ -1,19 +1,19 @@
 import { useState } from "react";
 import styled from "@emotion/styled";
+import getPrompt from "../../hooks/usePrompt";
 
 const Chat = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   gap: 32px;
-  height: auto;
-  padding: 100px;
+  height: 100vh;
+  padding-top: 50px;
   margin-top: 20px;
-  border: 1px solid #ccc;
 `;
 
 const Input = styled.input`
-  width: 50%;
+  width: 700px;
   padding: 10px;
   border-radius: 5px;
   border: 1px solid #ccc;
@@ -22,36 +22,37 @@ const Input = styled.input`
 const Response = styled.textarea`
   width: 50%;
   padding: 10px;
+  height: auto;
   border-radius: 5px;
-  border: 1px solid #ccc;
-  resize: none;
-  pointer-events: none;
-  outline: none;
+  border: none;
 `;
 
 export default function PromptAndResponse() {
   const [response, setResponse] = useState("");
-
-  // Imagina que esta función se conecta con la API de OpenAI y obtiene la respuesta
-  const getAIResponse = async (prompt) => {
-    // Aquí iría el código para obtener la respuesta de la API de OpenAI
-    // Por ahora, solo devolveremos una respuesta simulada
-    console.log(prompt);
-    return "Response...";
-  };
+  const [textInput, setTextInput] = useState("");
 
   const handleInputChange = async (event) => {
-    const prompt = event.target.value;
-    const aiResponse = await getAIResponse(prompt);
-    setResponse(aiResponse);
+    setTextInput(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const text = await getPrompt(textInput);
+    console.log("textito", text);
+    if (!text) alert("Algo salio mal");
+    else setResponse(text);
   };
 
   return (
     <Chat>
-      <Input
-        placeholder="How can I teach my cat not to climb on the sofa?..."
-        onChange={handleInputChange}
-      />
+      <form onSubmit={handleSubmit}>
+        <Input
+          placeholder="How can I teach my cat not to climb on the sofa?..."
+          onChange={handleInputChange}
+        />
+        <button type="submit">Send</button>
+      </form>
+      <span>Response</span>
       <Response value={response} readOnly />
     </Chat>
   );
