9ea78ad7294b156d166f39b0c62611f1437839d4
hernancur <hernangarcia@G500606MAC829.local>
2024-02-21 11:52:12 -0300

    Initialized new backend

--- api/src/db.js
+++ api/src/db.js
@@ -4,16 +4,16 @@ const { Sequelize } = require("sequelize");
 
 const db = {};
 
-const sequelize = new Sequelize({
-  host: process.env.DATABASE_HOST,
-  dialect: process.env.DIALECT,
-  port: process.env.DATABASE_PORT,
-  password: process.env.DATABASE_PASSWORD,
-  username: process.env.DATABASE_USERNAME,
-  database: process.env.DATABASE_NAME,
-  define: { timestamps: false },
-});
-
+// const sequelize = new Sequelize({
+//   host: process.env.DATABASE_HOST,
+//   dialect: process.env.DIALECT,
+//   port: process.env.DATABASE_PORT,
+//   password: process.env.DATABASE_PASSWORD,
+//   username: process.env.DATABASE_USERNAME,
+//   database: process.env.DATABASE_NAME,
+//   define: { timestamps: false },
+// });
+const sequelize = new Sequelize(process.env.MYSQL_URL);
 const User = require("./lib/users/users.model")(sequelize, Sequelize.DataTypes);
 const Pets = require("./lib/pets/pets.model")(sequelize, Sequelize.DataTypes);
 
@@ -25,8 +25,8 @@ User.hasMany(Pets);
 Pets.belongsTo(User);
 
 const dbInit = async () => {
-  await Pets.sync({ alter: true });
   await User.sync({ alter: true });
+  await Pets.sync({ alter: true });
 };
 
 module.exports = {
